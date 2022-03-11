586ab48d7384c4e4ce74217cf0c228d4615b3556
qianxi0410 <894871277@qq.com>
2022-03-11 10:26:58 +0800

    feat: auto update log

--- main.go
+++ main.go
@@ -155,7 +155,7 @@ func main() {
 		log.Fatalf("unmarshal response failed. %v", err)
 	}
 
-	path := "./report.log"
+	path := "report.log"
 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("error opening file: %v", err)
