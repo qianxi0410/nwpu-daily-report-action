af1667810090f931d879cb6b22227c840e36271f
qianxi0410 <894871277@qq.com>
2022-09-06 20:23:00 +0800

    feat: update some fields

--- type.go
+++ type.go
@@ -24,11 +24,15 @@ type (
 		Szcsbm      string `naive:"szcsbm" default:"1"`
 		Bdzt        string `naive:"bdzt" default:"1"`
 		Szcsmc      string `naive:"szcsmc" default:"在学校"`
+		Szcsmc1     string `naive:"szcsmc1" default:"在学校"`
 		Sfyzz       string `naive:"sfyzz" default:"0"`
 		Sfqz        string `naive:"sfqz" default:"0"`
-		Tbly        string `naive:"tbly" default:"sso"`
+		Tbly        string `naive:"tbly" default:"pc"`
 		Qtqksm      string `naive:"qtqksm"`
 		Ycqksm      string `naive:"ycqksm"`
+		Sfxn        string `naive:"sfxn" default:"0"`
+		Sfdw        string `naive:"sfdw" default:"0"`
+		Longlat     string `naive:"longlat"`
 		UserType    string `naive:"userType" default:"2"`
 		Username    string `naive:"userName"`
 	}
